2495ef7812c1f71bddf5f794dc6765014c5b566e
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-06-12 20:56:02 +1000

    Updates

---
+++ precache-manifest.e52c7bdaf22f2e770226abd56518574f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f56f253bfe90719f321ea7f8e7eef5",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "d17cd65f5b3bd6b02d9d",
+    "url": "/weather-app/static/css/main.7178d054.chunk.css"
+  },
+  {
+    "revision": "6f9da207b79e184da8ff",
+    "url": "/weather-app/static/js/2.095184d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.095184d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d17cd65f5b3bd6b02d9d",
+    "url": "/weather-app/static/js/main.2952be0e.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  },
+  {
+    "revision": "44aface148703b249a77b17d3f7e65bb",
+    "url": "/weather-app/static/media/cold.44aface1.jpg"
+  },
+  {
+    "revision": "320cf6f3d6c4f902fe59fd512f79c54c",
+    "url": "/weather-app/static/media/warm.320cf6f3.jpg"
+  }
+]);
